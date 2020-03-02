db14b3bd0c1050123a35a798b60fbe31063daba9
axmd97 <asgarova98@list.ru>
2020-03-02 17:08:23 +0400

    chat

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var textUser = document.querySelector("#chatInput");
+textUser.addEventListener("keyup",function(e) {
+    var texti = textUser.value.trim();
+
+    if(texti != ""){
+        if (e.keyCode == 13) {
+            if (texti[0].toUpperCase() == texti[0]) {
+                AddMessage("support", texti)
+            }
+            else{
+                AddMessage("user",texti)
+            }
+        }
+    }
+});
+
+// function AddMessage(clName, texti) {
+//     var mDiv = document.createElement("div");
+//     mDiv.className = "message "+clName
+//     var ptag = document.createElement("p");
+//     ptag.innerText = texti;
+
+//     var date = new Date();
+//     var ptime = document.createElement("p")
+//     ptime.className = "time"
+//     ptime.innerText = date.getHours()+":"+date.getMinutes();
+//     mDiv.appendChild(ptag);
+//     mDiv.appendChild(ptime);
+//     document.querySelector("#chat main").appendChild(mDiv)
+//     textUser.value = ""
+// }
+
+
+
+function AddMessage(clName, texti){
+    var date = new Date();
+    document.querySelector("#chat main").insertAdjacentHTML('beforeend', `
+    <div class="message ${clName}">
+                <p>${texti}</p>
+          <p class="time">${date.getHours()+":"+date.getMinutes()}</p>
+            </div>
+            `)
+            textUser.value = ""
+}
