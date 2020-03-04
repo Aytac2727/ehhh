2d95a2b4d57605583d99f5062e0a612a743ea86e
Your Name <asgarova98@list.ru>
2020-03-04 12:53:55 +0400

    ehh

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 var textUser = document.querySelector("#chatInput");
-textUser.addEventListener("keyup",function(e) {
+textUser.addEventListener("keyup", function (e) {
     var texti = textUser.value.trim();
 
-    if(texti != ""){
+    if (texti != "") {
         if (e.keyCode == 13) {
             if (texti[0].toUpperCase() == texti[0]) {
                 AddMessage("support", texti)
             }
-            else{
-                AddMessage("user",texti)
+            else {
+                AddMessage("user", texti)
             }
         }
     }
+
 });
+
+var messageBox = document.querySelector(".message");
+messageBox.onclick = function(){
+    delete messageBox
+}
+
+
 
 // function AddMessage(clName, texti) {
 //     var mDiv = document.createElement("div");
@@ -30,15 +38,14 @@ textUser.addEventListener("keyup",function(e) {
 //     textUser.value = ""
 // }
 
-
-
-function AddMessage(clName, texti){
+function AddMessage(clName, texti) {
     var date = new Date();
     document.querySelector("#chat main").insertAdjacentHTML('beforeend', `
     <div class="message ${clName}">
                 <p>${texti}</p>
-          <p class="time">${date.getHours()+":"+date.getMinutes()}</p>
+          <p class="time">${date.getHours() + ":" + date.getMinutes()}</p>
             </div>
             `)
-            textUser.value = ""
+    textUser.value = ""
 }
+
